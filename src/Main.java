857ee4e2a20a394eb13d970f4a33709637d08721
Galymzhan1626 <231169@astanait.edu.kz>
2024-04-27 17:58:31 +0500

    assignment2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         myList.add(2);
         myList.add(3);
         myList.add(4);
+        myList.add(5);
+        myList.add(6);
+        myList.addFirst(7);
 
-        myList.add();
+        System.out.println(myList.getFirst());
+        System.out.println(myList.get(1));
+        System.out.println(myList.getLast());
+        System.out.println(myList.indexOf(7));
+
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Iterator;
 
 public class MyArrayList<T> implements MyListInterface{
@@ -14,13 +16,6 @@ public class MyArrayList<T> implements MyListInterface{
         }
         arr[size++] = (T) item;
     }
-    private void increaseBuffer() {
-        T[] newArr = (T[]) new Object[arr.length*2];
-        for (int i = 0; i < arr.length; i++) {
-            newArr[i] = arr[i];
-        }
-        arr = newArr;
-    }
     @Override
     public void set(int index, Object item) {
         arr[index]= (T) item;
@@ -28,87 +23,163 @@ public class MyArrayList<T> implements MyListInterface{
 
     @Override
     public void add(int index, Object item) {
-        arr[index]= (T) item;
+        checkIndex(index);
+        if (size >= arr.length) {
+            increaseBuffer();
+        }
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
+        }
+
+        arr[index] = (T) item;
+        size++;
     }
+
 
     @Override
     public void addFirst(Object item) {
-        for (int i = size-1; i < 0; i--) {
-            arr[i+1]=arr[i];
+        if (size >= arr.length) {
+            increaseBuffer();
         }
-        arr[0]=(T) item;
+        for (int i = size; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = (T) item;
+        size++;
     }
+
 
     @Override
     public void addLast(Object item) {
-        arr[arr.length-1]= (T) item;
+        if (size >= arr.length) {
+            increaseBuffer();
+        }
+
+        arr[size] = (T) item;
+        size++;
+    }
+
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return arr[index];
     }
 
     @Override
-    public Object get(int index) {
-
-        System.out.println( arr[index]);
-        return null;
-    }
-
-    @Override
-    public Object getFirst() {
+    public T getFirst() {
         return arr[0];
     }
 
     @Override
-    public Object getLast() {
-        return arr[-1];
+    public T getLast() {
+        return arr[size-1];
     }
 
     @Override
     public void remove(int index) {
-
+        checkIndex(index);
+        for (int i = index; i < size-1; i++) {
+            arr[i]=arr[i+1];
+        }
+        size--;
     }
 
     @Override
     public void removeFirst() {
-
+        if (size == 0) {
+            System.out.println("List is empty");
+        }
+        remove(0);
     }
 
     @Override
     public void removelast() {
-
+        if (size == 0) {
+            System.out.println("List is empty");
+        }
+        remove(size - 1);
     }
 
     @Override
     public void sort() {
-
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (((Comparable<T>) arr[j]).compareTo((T) arr[j + 1]) > 0) {
+                    T temp = (T) arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public int indexOf(Object object) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i]==object) {
+                return i;
+            }
+
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object object) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+        if (arr[i]==object) {
+            return i;
+        }
+    }
+        return -1;
+
     }
 
     @Override
     public boolean exists(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i]==object) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] newArray = new Object[size];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = arr[i];
+        }
+        return newArray;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            arr[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void increaseBuffer() {
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is not correct");
+        }
     }
 
     @Override
